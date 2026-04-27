98125da043e73b338e9105404d55503b5156c850
Linling06 <xie25001@byui.edu>
2026-04-26 22:50:59 -0600

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter the grade percentage: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade letter is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation! You have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass the course, maybe next time!");
+        }
+
+
+
     }
 }
