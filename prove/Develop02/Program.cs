6f8fd4bce446bfd9da0bcb1ebbb642744735208e
Linling06 <xie25001@byui.edu>
2026-05-17 00:06:54 -0600

    finshed some code for prove

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        JournalEntry myJournalEntry = new JournalEntry();
+        myJournalEntry.CreateJournalEntry();
+        // myJournalEntry.DisplayJournalEntry();
+        // Console.WriteLine(myJournalEntry.CreateFileSystemString());
+        JournalEntry myJournalEntry2 = new JournalEntry();
+        myJournalEntry2.CreateJournalEntry();
+
+        Journal myJournal = new Journal();
+        myJournal.AddJournalEntry(myJournalEntry);
+        myJournal.AddJournalEntry(myJournalEntry2);
+
+        myJournal.DisplayJournal();
     }
 }
