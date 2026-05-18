033528af393267abf6103c1758ca7614a1b9039f
Linling06 <xie25001@byui.edu>
2026-05-18 16:29:20 -0600

    add some code and fix bugs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,18 +1,59 @@
+using System.Text.Json;
+
 class Journal
 {
-    
-    List<JournalEntry>  _journalEntries = new List<JournalEntry>();
-
-    public void AddJournalEntry(JournalEntry journalEntry)
+    List<Entry> _entries = new List<Entry>();
+    public void Write()
     {
-        _journalEntries.Add(journalEntry);
+        string prompt = PromptGenerator.GetPrompt();
+        Console.WriteLine(prompt);
+
+        // Prompt the user for their answer.
+        Console.Write("> ");
+        string answer = Console.ReadLine();
+
+        // Add the entry.
+        _entries.Add(new Entry(prompt, answer));
     }
 
-    public void DisplayJournal()
+    public void Display()
     {
-        foreach (JournalEntry journalEntry in _journalEntries)
+        foreach (Entry entry in _entries)
         {
-            journalEntry.DisplayJournalEntry();
+            Console.WriteLine(entry.DisplayEntry());
+            Console.WriteLine();
         }
+    }
+
+    public void LoadEntries()
+    {
+        Console.Write("What is the journal name? ");
+        string dataPath = Console.ReadLine() + ".json";
+
+        if (File.Exists(dataPath))
+        {
+            _entries = JsonSerializer.Deserialize<List<Entry>>(File.ReadAllText(dataPath)) ?? new List<Entry>();
+            Console.WriteLine($"Journal loaded successfully!");
+        }
+        else
+        {
+            Console.WriteLine("That journal does not exist.");
+        }
+    }
+
+    public void SaveEntries()
+    {
+        Console.Write("What is the journal name? ");
+        string dataPath = Console.ReadLine() + ".json";
+
+        File.WriteAllText(
+            dataPath,
+            JsonSerializer.Serialize(
+                _entries,
+                new JsonSerializerOptions { WriteIndented = true }
+            )
+        );
+
+        Console.WriteLine("Journal saved successfully!");
     }
 }
