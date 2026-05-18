3918c1d2f0beb0843b8dc7d89c03a485ef13c8ba
Linling06 <xie25001@byui.edu>
2026-05-18 06:41:36 -0600

    upload code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+
+    void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    void PromtUserBirthYear(out int year)
+    {
+        Console.WriteLine("Please enter the year you were born: ");
+        int year = int.Parse(Console.ReadLine());
+    }
+
     static void Main(string[] args)
     {
+        int year;
         Console.WriteLine("Hello Prep5 World!");
     }
 }
