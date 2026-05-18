6290e8f1bd914773b4bd2ad1067e607962786515
Linling06 <xie25001@byui.edu>
2026-05-18 16:20:29 -0600

    fix bugs and add code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,69 +1,21 @@
-// Exceeding Requirements:
-
-// I prompt the user for the journal name to save the file and save it in a
-// JSON format; this will make the program more user-friendly and make sure the
-// data is more structured. I also did a lot of crash prevention to make the
-// program more stable.
-
-
 using System;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Initialize a journal.
-        Journal journal = new Journal();
+        JournalEntry myJournalEntry = new JournalEntry();
+        myJournalEntry.CreateJournalEntry();
+        // myJournalEntry.DisplayJournalEntry();
+        // Console.WriteLine(myJournalEntry.CreateFileSystemString());
+        JournalEntry myJournalEntry2 = new JournalEntry();
+        myJournalEntry2.CreateJournalEntry();
 
-        // Welcome message.
-        Console.WriteLine("Welcome to the Journal Program!");
-        
-        // Program loop.
-        int menuNumber;
-        do
-        {
-            menuNumber = GetMenuNumber();
-            switch (menuNumber)
-            {
-                case 1:
-                    journal.Write();
-                    break;
-                case 2:
-                    journal.Display();
-                    break;
-                case 3:
-                    journal.LoadEntries();
-                    break;
-                case 4:
-                    journal.SaveEntries();
-                    break;
-            }
-        } while (menuNumber != 5);
+        Journal myJournal = new Journal();
+        myJournal.AddJournalEntry(myJournalEntry);
+        myJournal.AddJournalEntry(myJournalEntry2);
 
-        // Goodbye message.
-        Console.WriteLine("Goodbye!");
+        myJournal.DisplayJournal();
     }
-
-    static int GetMenuNumber()
-    {
-        Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.Write("What would you like to do? ");
-        while (true)
-        {
-            if (int.TryParse(Console.ReadLine(), out int userNumber) && userNumber is >=1 and <= 5)
-            {
-                return userNumber;
-            }
-            else
-            {
-                Console.Write("That's not a valid input, please try again: ");
-            }
-        }
-    }
-
 }
---
+++ sandbox/Angle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+
+        Angle myAngle = new Angle();
+        myAngle.SetRadians(10);
+        Console.WriteLine(myAngle.GetRadians());
+    }
+}
