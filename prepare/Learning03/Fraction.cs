870fcfb5434e02b91ee814134fec84c9e3e9e7a4
Linling06 <xie25001@byui.edu>
2026-06-01 09:49:08 -0600

    Encapsulation Learning Activity finished

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,64 @@
+using System.Globalization;
+
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        SetBottom(bottom);
+    }
+
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            _bottom = bottom;
+        }
+        else
+        {
+            _bottom = 1;
+        }
+    }
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is will be recomputed each time this is called.
+        return (double)_top / (double)_bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+
+        Random random = new Random();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            int top = random.Next(1, 11);
+            int bottom = random.Next(1, 11);
+
+            fraction.SetTop(top);
+            fraction.SetBottom(bottom);
+
+            Console.WriteLine(
+                $"Fraction {i}: string: {fraction.GetFractionString()} Number: {fraction.GetDecimalValue()}"
+            );
+        }
+
+
     }
 }
