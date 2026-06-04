e0fd37f13fb7ebc1a5e9145b14d04ea6edc9a6d4
Linling06 <xie25001@byui.edu>
2026-06-04 15:26:11 -0600

    w07day02 in class

---
+++ sandbox/Person/Doctor.cs
@@ -0,0 +1,19 @@
+class Doctor : Person
+{
+    private string _degree;
+
+    public Doctor(string degree, string firstName,
+        string lastName, int age, int weight)
+        : base(firstName, lastName, age, weight)
+    {
+        _degree = degree;
+    }
+
+    public string GetDoctorInformation()
+    {
+        return $"Education: {_degree}, {GetPersonInformation()}";
+    }
+
+
+
+}
